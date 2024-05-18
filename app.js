704b196b28dca8a00957841098c6baecb8da9c9e
Maximus <hey@m4xy.org>
2024-05-18 11:12:26 +0530

    fix

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-// function myFunction() {
-//     let element = document.body;
-//     element.classList.toggle("dark");
-//     var x = document.getElementById("myDIV");
-//     if (x.innerHTML === "Dark") {
-//         x.innerHTML = "Light";
-//     } else {
-//         x.innerHTML = "Dark";
-//     }
-
-
-
-//     let body = document.body;
-
-//     x.addEventListener('change', function () {
-//         localStorage.setItem('dark', this.checked);
-//         if (this.checked) {
-//             body.classList.add('dark')
-//         } else {
-//             body.classList.remove('dark')
-//         }
-
-//         if (localStorage.getItem('dark')) {
-//             x.classList.add('dark');
-//         }
-//     })
-// };
-
 const x = document.getElementById('myDIV');
 
 const body = document.body;
